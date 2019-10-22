0243cb6f3d2124615fd2093da46085fea456a022
Guglielmo Felici <guglielmo.felici@yahoo.it>
2019-10-22 19:04:43 +0200

    initial commit

---
+++ data/data.js
@@ -0,0 +1,62 @@
+const db =
+{
+    "queue": [
+        "dano",
+        "ceccherini",
+        "caciotta",
+        "sanchez",
+        "jala",
+        "pogre",
+        "iroli",
+        "fosq",
+        "paglia"
+    ],
+    "users": {
+        "dano": {
+            "nickname": "Dano",
+            "img": "dano.jpg",
+            "bio": "Chin chon chun mi scopo i parenti e i morti"
+        },
+        "ceccherini": {
+            "nickname": "Ceccherini",
+            "img": "ceccherini.jpg",
+            "bio": "Hell no"
+        },
+        "caciotta": {
+            "nickname": "Guglielmo il diarca bello",
+            "img": "caciotta.jpg",
+            "bio": "Vi cano il pano"
+        },
+        "sanchez": {
+            "nickname": "Sanchez il diarca simpatico",
+            "img": "sanchez.jpg",
+            "bio": "Oh zi caccia il commander"
+        },
+        "jala": {
+            "nickname": "Jalapegno",
+            "img": "jala.jpg",
+            "bio": "Il mio nome... è Jalapegno."
+        },
+        "pogre": {
+            "nickname": "Pogre",
+            "img": "pogre.jpg",
+            "bio": "L'arte è molto worth"
+        },
+        "iroli": {
+            "nickname": "Iroli",
+            "img": "iroli.jpg",
+            "bio": "Eeeeeh Platone..."
+        },
+        "fosq": {
+            "nickname": "Fosq",
+            "img": "fosq.jpg",
+            "bio": "Un saluto a mia zia Graziana"
+        },
+        "paglia": {
+            "nickname": "Paglia",
+            "img": "paglia.jpg",
+            "bio": "..."
+        },
+    },
+
+};
---
+++ js/queue.js
@@ -0,0 +1,15 @@
+const section = "QUEUE";
+$(document).ready(function () {
+    let queue = $("#queue");
+    $.each(db.queue, (i) => {
+        let user = db.users[db.queue[i]];
+        $("#queue").append(
+            $("<li/>").append(
+                $("<img/>")
+                    .attr("src", (i, old) => `../data/images/${user.img}`),
+                `<h2>${user.nickname}</h2>`,
+                `<p>${user.bio}</p>`,
+            ).attr("class", (x, old) => i === 0 ? "master" : "")
+        );
+    })
+});
