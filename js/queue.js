8543cc734d91915d53094768ea582927e34ae1cd
GuglielmoFelici <guglielmo.felici@yahoo.it>
2019-11-15 16:53:02 +0100

    test read from json

--- js/queue.js
+++ js/queue.js
@@ -2,27 +2,24 @@ const section = "QUEUE";
 $(document).ready(function () {
     let queue = $("#queue");
     console.log('request');
-    const htmlQueue = $.get('https://guglielmofelici.github.io/niccolgur/data/prova.json', (response) => {
-        console.log(response);
-        const queueDb = JSON.parse(response);
-        console.log(queueDb);
+    const htmlQueue = $.get('https://guglielmofelici.github.io/niccolgur/data/prova.json', (queueDb) => {
+        $.each(queueDb, (i) => {
+            console.log(i);
+            let user = db.users[queueDb[i]];
+            $("#queue").append(
+                $("<li/>")
+                .append(
+                    $("<img/>")
+                    .attr("src", `../data/images/${user.img}`),
+                    `<h2>${user.nickname}</h2>`,
+                    `<p>${user.bio}</p>`
+                )
+                .addClass((x, old) => i === 0 ? "master" : "")
+                .addClass("highlighted")
+                .css("opacity", 0)
+            );
+        });
     })
-
-    $.each(db.queue, (i) => {
-        let user = db.users[db.queue[i]];
-        $("#queue").append(
-            $("<li/>")
-            .append(
-                $("<img/>")
-                .attr("src", `../data/images/${user.img}`),
-                `<h2>${user.nickname}</h2>`,
-                `<p>${user.bio}</p>`
-            )
-            .addClass((x, old) => i === 0 ? "master" : "")
-            .addClass("highlighted")
-            .css("opacity", 0)
-        );
-    });
 
     $("li").addClass("highlighted");
 
