2988b3855f69322500ea53ebe98180f3a33ae96e
GuglielmoFelici <guglielmo.felici@yahoo.it>
2019-11-19 18:21:22 +0100

    pages

--- mybuild.bat
+++ mybuild.bat
@@ -1,6 +1,6 @@
 call ng build --prod --output-path docs --base-href "niccolgur"
-call cp docs/index.html docs/404.html
-call cp -r data docs/data
+cp docs/index.html docs/404.html
+cp -r data docs/data
 call git add .
 call git commit -m "pages"
 call git push
