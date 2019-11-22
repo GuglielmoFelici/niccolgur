8eb816a4172c0fd40ae88ee5394c23f893da63ff
GuglielmoFelici <guglielmo.felici@yahoo.it>
2019-11-22 16:27:12 +0100

    pages

--- mybuild.bat
+++ mybuild.bat
@@ -1,4 +1,4 @@
-call ng build --prod --output-path docs --base-href "porcodio"
+call ng build --prod --output-path docs --deploy-url /niccolgur/ --base-href /niccolgur/
 call powershell cp docs/index.html docs/404.html
 call powershell cp -r data docs/data
 call git add .
