4ba148bb68d52cce8188496a9a78988daae92f0f
Guglielmo Felici <guglielmo.felici@protonmail.com>
2020-06-05 11:42:56 +0200

    pages

--- src/app/ts/env.ts
+++ src/app/ts/env.ts
@@ -2,7 +2,7 @@ export const apiKey = '8dd0d0ce432b75b532e9fb563fa8fded';
 
 /******************************************* Endpoints *************************************************/
 /* Niccolgur */
-export const queue = 'http://93.48.175.17/queue/full';
+export const queue = 'https://93.48.175.17/queue/full';
 export const users = 'https://guglielmofelici.github.io/niccolgur/data/users.json';
 export const niccolgurs = 'https://guglielmofelici.github.io/niccolgur/data/niccolgurs.json';
 export const images = 'https://guglielmofelici.github.io/niccolgur/data/images/';
