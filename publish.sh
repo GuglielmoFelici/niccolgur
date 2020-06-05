05da4eb54d7544adee797af3d7b1bca98e03fd3d
Guglielmo Felici <guglielmo.felici@protonmail.com>
2020-06-06 01:04:04 +0200

    backup

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 node_modules/.bin/ng build --prod --output-path docs # --base-href /niccolgur/
 cp docs/index.html docs/404.html
+cp CNAME docs/
 git add .
 git commit -m "pages"
 git push
